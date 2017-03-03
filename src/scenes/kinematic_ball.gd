7edd6c63a7c5f4e90b7d68d6fba9292f9b841a74
j <l@l>
2017-03-03 17:13:01 -0600

    tweak gravity a bit, still have issues

--- src/scenes/kinematic_ball.gd
+++ src/scenes/kinematic_ball.gd
@@ -6,8 +6,7 @@ const SPEED_INCREMENT = 50
 const GRAV_CONST = 1
 
 var move_vector = Vector2(3, 1)
-var grav_mass = 100
-var speed = 100
+var grav_mass = 1
 
 func _ready():
 	set_fixed_process(true)
@@ -15,14 +14,20 @@ func _ready():
 
 func _fixed_process(delta):
 	var grav_vector = get_gravity_vector(get_node("../player_1").get_pos(), get_node("../player_1").grav_mass)
-	var move_vector_tick = move_vector.normalized() * speed
-	move_vector_tick += grav_vector
-	print(grav_vector)
-	var collision_vector = move(move_vector_tick * delta)
+	move_vector += grav_vector
+	var collision_vector = move(move_vector * delta)
 	
 	if is_colliding():
+		print("Collision detected: ", collision_vector)
+		print("Move vector before: ", move_vector)
 		move_vector = get_collision_normal().reflect(collision_vector)
+		print("Move vector after: ", move_vector)
+		move(move_vector)
 
 func get_gravity_vector(player_vector, player_mass):
+	var accel = Vector2()
 	var force = -GRAV_CONST * ( (player_mass * grav_mass) / get_global_pos().distance_squared_to(player_vector) ) * (get_global_pos() - player_vector).normalized()
+	accel.x = force.x / grav_mass
+	accel.y = force.y / grav_mass
+	
 	return force
--- src/scripts/basic.gd
+++ src/scripts/basic.gd
@@ -3,24 +3,27 @@ extends Node2D
 var player_1_score = 0
 var player_2_score = 0
 
-var p1_start = Vector2(178, 231)
-var p2_start = Vector2(1024 - 178, 231)
+var p1_start = Vector2()
+var p2_start = Vector2()
 
 func _ready():
-	game_reset()
 	set_process(true)
+	
+	p1_start = get_node("player_1").get_global_pos()
+	p2_start = get_node("player_2").get_global_pos()
+	
+	game_reset()
 	
 	var p2_texture = preload("res://assets/p_red.png")
 	get_node("player_2").set_controller(2)
 	get_node("player_2/person_sprite").set_texture(p2_texture)
-	pass
 
 func _process(delta):
 	if get_node("kinematic_ball").get_pos().x < 0:
 		player_2_score += 1
 		get_node("player_2_score").set_text(str(player_2_score))
 		ball_reset()
-	elif get_node("kinematic_ball").get_pos().x > 1024:
+	elif get_node("kinematic_ball").get_pos().x > 1800:
 		player_1_score += 1
 		get_node("player_1_score").set_text(str(player_1_score))
 		ball_reset()
@@ -42,7 +45,6 @@ func ball_reset():
 
 func kine_ball_reset():
 	get_node("kinematic_ball").set_pos(Vector2(530, 300))
-	get_node("kinematic_ball").speed = 100
 	get_node("kinematic_ball").move_vector = Vector2(3, 1)
 
 func rigid_ball_reset():
